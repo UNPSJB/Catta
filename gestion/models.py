0d212d9f176ef8704dfbdfd2287c5cdb88cc26b9
Emiliano De Marco Andrada <emilianodemarcoandrada@gmail.com>
2016-10-03 17:50:16 -0300

    MIgraciones hechas de gestion

--- gestion/models.py
+++ gestion/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 class Sector(models.Model):
-    nombre = models.CharFIeld(max_length=100)
-    descripcion = models.CharFIeld(max_lenght=100)
+    nombre = models.CharField(primary_key=True, max_length=100)
+    descripcion = models.CharField(max_length=100)
 
-class Insumo
-    nombre = models.CharFIeld(max_length=100)
+class Insumo (models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
     contenidoNeto = models.IntegerField(default=0)
-    marca = models.CharFIeld(max_length=100)
+    marca = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
 
-class Servicio
-    nombre = models.CharFIeld(max_length=100)
-    descripcion = models.CharFIeld(max_length=100)
+class Servicio (models.Model):
+    nombre = models.CharField(primary_key=True, max_length=100)
+    descripcion = models.CharField(max_length=100)
     precio = models.IntegerField(default=0)
     duracion = models.IntegerField(default=0)
