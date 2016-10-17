74645f624be65696b174c6decaebeae05fb7faa7
Germán Bianchini <bianchiniger@gmail.com>
2016-10-17 01:44:46 -0300

    Valida el login para acceder a las páginas de dueño y empleado (Faltan permisos)

--- personas/views.py
+++ personas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 from personas.forms import CuentaNuevaForm, EmpleadoNuevoForm
 from gestion.forms import SectorForm, InsumoForm, ServicioForm
@@ -33,49 +34,52 @@ def empleado(request):
     return render(request, ret, {"form": form})
 
 
+@login_required(login_url='iniciar_sesion')
 def duenio(request):
-    if request.method == "POST":
-        form_clientes = CuentaNuevaForm(request.POST)
-        form_empleados = EmpleadoNuevoForm(request.POST)
-        form_sector = SectorForm(request.POST)
-        form_insumo = InsumoForm(request.POST)
-        form_servicio = ServicioForm(request.POST)
+    if request.user.is_authenticated:
+        print(request.user.is_authenticated)
+        if request.method == "POST":
+            form_clientes = CuentaNuevaForm(request.POST)
+            form_empleados = EmpleadoNuevoForm(request.POST)
+            form_sector = SectorForm(request.POST)
+            form_insumo = InsumoForm(request.POST)
+            form_servicio = ServicioForm(request.POST)
 
-        if 'crear_cuenta' in request.POST:
-            if form_clientes.is_valid():
-                form_clientes.save()
-                return redirect('duenio')
-        elif 'crear_empleado' in request.POST:
-            if form_empleados.is_valid():
-                print("antes")
-                form_empleados.save()
-                print("despues")
-                return redirect('duenio')
-        elif 'crear_sector' in request.POST:
-            if form_sector.is_valid():
-                form_sector.save()
-                return redirect('duenio')
-        elif 'crear_insumo' in request.POST:
-            if form_insumo.is_valid():
-                form_insumo.save()
-                return redirect('duenio')
-        elif 'crear_servicio' in request.POST:
-            if form_servicio.is_valid():
-                form_servicio.save()
-                return redirect('duenio')
-    else:
-        form_clientes = CuentaNuevaForm()
-        form_empleados = EmpleadoNuevoForm()
-        form_sector = SectorForm()
-        form_insumo = InsumoForm()
-        form_servicio = ServicioForm()
+            if 'crear_cuenta' in request.POST:
+                if form_clientes.is_valid():
+                    form_clientes.save()
+                    return redirect('duenio')
+            elif 'crear_empleado' in request.POST:
+                if form_empleados.is_valid():
+                    print("antes")
+                    form_empleados.save()
+                    print("despues")
+                    return redirect('duenio')
+            elif 'crear_sector' in request.POST:
+                if form_sector.is_valid():
+                    form_sector.save()
+                    return redirect('duenio')
+            elif 'crear_insumo' in request.POST:
+                if form_insumo.is_valid():
+                    form_insumo.save()
+                    return redirect('duenio')
+            elif 'crear_servicio' in request.POST:
+                if form_servicio.is_valid():
+                    form_servicio.save()
+                    return redirect('duenio')
+        else:
+            form_clientes = CuentaNuevaForm()
+            form_empleados = EmpleadoNuevoForm()
+            form_sector = SectorForm()
+            form_insumo = InsumoForm()
+            form_servicio = ServicioForm()
 
-    return render(request, 'duenio/index_duenio.html',
-                  {'form_clientes' : form_clientes ,
-                   'form_empleados' : form_empleados ,
-                   'form_sector' : form_sector,
-                   'form_insumo' : form_insumo,
-                   'form_servicio' : form_servicio })
+        return render(request, 'duenio/index_duenio.html',
+                      {'form_clientes' : form_clientes ,
+                       'form_empleados' : form_empleados ,
+                       'form_sector' : form_sector,
+                       'form_insumo' : form_insumo,
+                       'form_servicio' : form_servicio })
 
 
 def cliente(request):
