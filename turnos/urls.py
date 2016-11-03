11c5fe57e54701752dfd624b40f96b50a9da6aad
Germán Bianchini <bianchiniger@gmail.com>
2016-11-02 23:52:24 -0300

    Cambios visuales en la plantilla de la dueña

--- turnos/urls.py
+++ turnos/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^escupoJSON/$', views.escupoJSON, name='escupoJSON'),
     url(r'^modificar/$', views.modificar_turno, name='modifciar_turno'),
-    url(r'^eventosCalendario/$', views.eventosCalendario, name='eventosCalendario')
+    url(r'^escupoJSON/$', views.escupoJSON, name='escupoJSON')
 ]
--- turnos/views.py
+++ turnos/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse, HttpResponse
-from django.core.serializers.json import DjangoJSONEncoder
+from django.http import JsonResponse
 from turnos.forms import CrearTurnoForm, ModificarTurnoForm
 from turnos.models import Turno
 from django.core import serializers
@@ -27,18 +26,3 @@ def modificar_turno(request, id_turno=1):
         print('entre')
 
     return render(request, ret, {"form": form})
-
-# TODO pruebas con fullcalendar
-def eventosCalendario(request):
-    turnos = Turno.objects.all()
-
-    json_lista = []
-    for turno in turnos:
-        id_t = turno.id
-        title = turno.cliente.nombre
-        start = turno.fecha
-
-        json_item = {'id': id_t, 'title':title, 'start': start}
-        json_lista.append(json_item)
-
-    return HttpResponse(json.dumps(json_lista), content_type='turnos/json')
