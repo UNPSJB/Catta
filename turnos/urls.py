651c524176daa7b440be5dcd81146c878d2e7d01
Matias Acosta <matiasacosta1995@gmail.com>
2016-11-03 18:15:33 -0300

    Cambios en los formularios, views personas etc.

--- turnos/urls.py
+++ turnos/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^modificar/$', views.modificar_turno, name='modifciar_turno'),
     url(r'^escupoJSON/$', views.escupoJSON, name='escupoJSON'),
-    url(r'^devuelvo_turnos$', views.devuelvo_turnos, name='devuelvo_turnos'),
-    url(r'^confirmarTurno$', views.confirmarTurno, name='confirmarTurno')
+    url(r'^devuelvo_turnos$', views.devuelvo_turnos, name='devuelvo_turnos')
+
 ]
