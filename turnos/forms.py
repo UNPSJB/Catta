0573803e9b8a79c1101603022305f2834d394ecb
Matias Acosta <matiasacosta1995@gmail.com>
2017-12-19 13:15:16 -0300

    Cosas en la ayuda

--- turnos/forms.py
+++ turnos/forms.py
@@ -231,7 +231,6 @@ class CrearTurnoFijoForm(ModelForm):
         model = TurnoFijo
         fields = {"empleado","fecha", "cliente", "servicios", "fecha_fin"}
 
-
 #form_registrar_turno_realizado
 class RegistrarTurnoRealizadoForm(ModelForm):
     def __init__(self, *args, **kwargs):
